9da9a58a22def8b6f1088ee13c22f6b296751c8a
Mohamed-A-Badr <badr49960@gmail.com>
2023-02-13 21:20:41 +0200

    Protected Chair Data

--- routers/chair.py
+++ routers/chair.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, HTTPException, status, Depends
 import db.database as database, db.schemas as schemas, db.crud as crud, db.models as models
 from sqlalchemy.orm import Session
+from fastapi_jwt_auth import AuthJWT
 
 # * Here are the routes that related to the data coming from the chair
 
@@ -16,7 +17,17 @@ router = APIRouter(
     response_model=schemas.GetChairData,
     status_code=status.HTTP_200_OK,
 )
-async def get_chair_data(patient_id: int, db: Session = Depends(database.get_db)):
+async def get_chair_data(
+    patient_id: int,
+    authorize: AuthJWT = Depends(),
+    db: Session = Depends(database.get_db),
+):
+    try:
+        authorize.jwt_required()
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token"
+        )
     return crud.get_chair_data(patient_id=patient_id, db=db)
 
 
@@ -25,6 +36,14 @@ async def get_chair_data(patient_id: int, db: Session = Depends(database.get_db)
     "/data", response_model=schemas.ReadChairData, status_code=status.HTTP_202_ACCEPTED
 )
 async def read_new_chair_data(
-    data: schemas.ReadChairData, db: Session = Depends(database.get_db)
+    data: schemas.ReadChairData,
+    authorize: AuthJWT = Depends(),
+    db: Session = Depends(database.get_db),
 ):
+    try:
+        authorize.jwt_required()
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token"
+        )
     return crud.store_chair_data(data=data, db=db)

5c28dd87b63f82387d53f3155f122c74dfd98ea4
Mohamed-A-Badr <badr49960@gmail.com>
2023-03-10 01:26:53 +0200

    formatting chair and main files

--- routers/chair.py
+++ routers/chair.py
@@ -10,7 +10,7 @@ router = APIRouter(
     prefix="/chair",
 )
 
-'''
+"""
 # * Create a route that will return the last chair data for a specific patient
 @router.get(
     "/data",
@@ -30,6 +30,8 @@ async def get_chair_data(
     current_user = authorize.get_jwt_subject()
     return crud.get_chair_data(patient_id=current_user, db=db)
 
+"""
+
 
 # * Create a route that will store the data in the database when POST request is sent to the route
 @router.post("/data", status_code=status.HTTP_200_OK)
@@ -37,15 +39,17 @@ async def read_new_chair_data(
     data: schemas.ReadChairData, db: Session = Depends(database.get_db)
 ):
     return crud.store_chair_data(data=data, db=db)
-'''
 
-# * Register a new chair to use in the database z
+
+# * Register a new chair to use in the database
 @router.post("/signup", status_code=status.HTTP_200_OK)
 async def chair_registration(
     chair: schemas.ChairRegistration, db: Session = Depends(database.get_db)
 ):
     return crud.chair_signup(chair=chair, db=db)
 
+
+# * Caregiver login to the chair to access sensor's data
 @router.post("/login", status_code=status.HTTP_200_OK)
 async def chair_login(
     chair: schemas.ChairRegistration, db: Session = Depends(database.get_db)
--- test_main.py
+++ test_main.py
@@ -3,9 +3,8 @@ from main import app
 
 client = TestClient(app)
 
+
 def test_root():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {
-        "message":"Hello World"
-    }
+    assert response.json() == {"message": "Hello World"}

ed500283610ef0536ec8e2684ff46bdca5c8e2d0
Mohamed-A-Badr <badr49960@gmail.com>
2023-03-09 23:18:44 +0200

    Create chair and patient models

--- db/models.py
+++ db/models.py
@@ -1,31 +1,44 @@
-from sqlalchemy import Column, String, Integer, Float, ForeignKey, DateTime
+from sqlalchemy import Column, String, Integer, Float, ForeignKey, Time, Date
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 from db.database import Base
 
 
+class Chair(Base):
+    __tablename__ = "chair"
+
+    id = Column(Integer, primary_key=True)
+    password = Column(String)
+
+    # ? relationship with sensor_data
+    sensor_data = relationship("SensorData", back_populates="chair")
+
+
+class SensorData(Base):
+    __tablename__ = "sensor_data"
+
+    id = Column(Integer, primary_key=True)
+    temperature = Column(Float)
+    pulse_rate = Column(Float)
+    oximeter = Column(Float)
+    sugar_level = Column(Float)
+    created_date = Column(Date, default=func.current_date())
+    created_time = Column(Time, server_default=func.current_time())
+
+    # ? relationship with chair
+    chair_id = Column(Integer, ForeignKey("chair.id"))
+    chair = relationship("Chair", back_populates="sensor_date")
+
+
 class Patient(Base):
     __tablename__ = "patient"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, unique=True, index=True)
     first_name = Column(String)
     last_name = Column(String)
-    age = Column(Integer)
     gender = Column(String)
+    age = Column(String)
 
-    chair_data = relationship("ChairData", back_populates="patient")
-
-
-class ChairData(Base):
-    __tablename__ = "chair_data"
-
-    id = Column(Integer, primary_key=True, index=True)
-    password = Column(String)
-    body_temperature = Column(Float, nullable=False)
-    oximeter = Column(Float, nullable=False)
-    heart_rate = Column(Float, nullable=False)
-    sugar_level = Column(Float, nullable=False)
-    created_at = Column(DateTime(timezone=True), server_default=func.now())
-
-    patient_id = Column(Integer, ForeignKey("patient.id"))
-    patient = relationship("Patient", back_populates="chair_data")
+    # ? one to one relationship with chair
+    chair_id = Column(Integer, ForeignKey("chair.id"))
+    chair = relationship("Chair", uselist=False, back_populates="patient")

b2eaa7ffacb4ae5316e98ad2b7ebab2027be8e21
Mohamed-A-Badr <badr49960@gmail.com>
2023-06-24 05:16:52 +0300

    Merging related tables in the same screen in admin panel

--- adminPanel/panel/admin.py
+++ adminPanel/panel/admin.py
@@ -3,9 +3,36 @@ from .models import Patient, Chair, Caregiver, Caregiverphone, SensorData, Locat
 
 # Register your models here.
 
+
+class PhoneNumberInline(admin.TabularInline):
+    model = Caregiverphone
+    extra = 0
+
+
+class CaregiverAdmin(admin.ModelAdmin):
+    inlines = [
+        PhoneNumberInline,
+    ]
+
+
+class LocationInline(admin.TabularInline):
+    model = Location
+    extra = 0
+
+
+class SensorDataInline(admin.TabularInline):
+    model = SensorData
+    extra = 0
+
+
+class ChairAdmin(admin.ModelAdmin):
+    inlines = [
+        LocationInline,
+        SensorDataInline,
+    ]
+
+
 admin.site.register(Patient)
-admin.site.register(Caregiver)
-admin.site.register(Caregiverphone)
-admin.site.register(Chair)
+admin.site.register(Caregiver, CaregiverAdmin)
+admin.site.register(Chair, ChairAdmin)
 admin.site.register(SensorData)
-admin.site.register(Location)
